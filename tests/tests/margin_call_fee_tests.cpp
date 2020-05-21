fa67f826afccf390e4bd50801e09cc48f5a4300f
Michel Santos <MichelSantos@users.noreply.github.com>
2020-05-21 14:04:26 -0400

    BSIP74: Test prevention of early usage of margin call fee ratio (MCFR)

--- tests/tests/margin_call_fee_tests.cpp
+++ tests/tests/margin_call_fee_tests.cpp
@@ -691,4 +691,394 @@ BOOST_FIXTURE_TEST_SUITE(margin_call_fee_tests, bitasset_database_fixture)
    }
 
 
+   /**
+    * Test the ability to create and update assets with a margin call fee ratio (MCFR) before HARDFORK_CORE_BSIP74_TIME
+    *
+    *
+    * Before HARDFORK_CORE_BSIP74_TIME
+    *
+    * 1. Asset owner fails to create the smart coin called USDBIT with a MCFR
+    * 2. Asset owner fails to create the smart coin called USDBIT with a MCFR in a proposal
+    * 3. Asset owner succeeds to create the smart coin called USDBIT without a MCFR
+    *
+    * 4. Asset owner fails to update the smart coin with a MCFR
+    * 5. Asset owner fails to update the smart coin with a MCFR in a proposal
+    *
+    *
+    * 6. Activate HARDFORK_CORE_BSIP74_TIME
+    *
+    *
+    * After HARDFORK_CORE_BSIP74_TIME
+    *
+    * 7. Asset owner succeeds to create the smart coin called CNYBIT with a MCFR
+    * 8. Asset owner succeeds to create the smart coin called RUBBIT with a MCFR in a proposal
+    *
+    * 9. Asset owner succeeds to update the smart coin called CNYBIT with a MCFR
+    * 10. Asset owner succeeds to update the smart coin called RUBBIT with a MCFR in a proposal
+    *
+    * 11. Asset owner succeeds to create the smart coin called YENBIT without a MCFR
+    * 12. Asset owner succeeds to update the smart coin called RUBBIT without a MCFR in a proposal
+    */
+   BOOST_AUTO_TEST_CASE(prevention_before_hardfork_test) {
+      try {
+         ///////
+         // Initialize the scenario
+         ///////
+         generate_blocks(db.get_dynamic_global_properties().next_maintenance_time);
+         trx.clear();
+         set_expiration(db, trx);
+
+         // Create actors
+         ACTORS((assetowner));
+
+         // CORE asset exists by default
+         asset_object core = asset_id_type()(db);
+         const asset_id_type core_id = core.id;
+
+         // Fund actors
+         uint64_t initial_balance_core = 10000000;
+         transfer(committee_account, assetowner.id, asset(initial_balance_core));
+
+         // Confirm before hardfork activation
+         BOOST_CHECK(db.head_block_time() < HARDFORK_CORE_BSIP74_TIME);
+
+
+         ///////
+         // 1. Asset owner fails to create the smart coin called bitUSD with a MCFR
+         ///////
+         const uint16_t market_fee_percent = 2 * GRAPHENE_1_PERCENT;
+         const optional<uint16_t> icr_opt = {}; // Initial collateral ratio
+         const uint16_t mcfr_5 = 50; // 5% expressed in terms of GRAPHENE_COLLATERAL_RATIO_DENOM
+         optional<uint16_t> mcfr_opt = mcfr_5;
+
+         // Attempt to create the smart asset with a MCFR
+         // The attempt should fail because it is before HARDFORK_CORE_BSIP74_TIME
+         {
+            const asset_create_operation create_op = make_bitasset("USDBIT", assetowner.id, market_fee_percent,
+                                                                   charge_market_fee, 4, core_id,
+                                                                   GRAPHENE_MAX_SHARE_SUPPLY, icr_opt, mcfr_opt);
+            trx.clear();
+            trx.operations.push_back(create_op);
+            sign(trx, assetowner_private_key);
+            REQUIRE_EXCEPTION_WITH_TEXT(PUSH_TX(db, trx), "cannot be set before Hardfork BSIP74");
+         }
+
+         ///////
+         // 2. Asset owner fails to create the smart coin called bitUSD with a MCFR in a proposal
+         ///////
+         {
+            const asset_create_operation create_op = make_bitasset("USDBIT", assetowner.id, market_fee_percent,
+                                                                   charge_market_fee, 4, core_id,
+                                                                   GRAPHENE_MAX_SHARE_SUPPLY, icr_opt, mcfr_opt);
+            proposal_create_operation cop;
+            cop.review_period_seconds = 86400;
+            uint32_t buffer_seconds = 60 * 60;
+            cop.expiration_time = db.head_block_time() + *cop.review_period_seconds + buffer_seconds;
+            cop.fee_paying_account = GRAPHENE_TEMP_ACCOUNT;
+            cop.proposed_ops.emplace_back(create_op);
+
+            trx.clear();
+            trx.operations.push_back(cop);
+            // sign(trx, assetowner_private_key);
+            REQUIRE_EXCEPTION_WITH_TEXT(PUSH_TX(db, trx), "cannot be set before Hardfork BSIP74");
+         }
+
+
+         ///////
+         // 3. Asset owner succeeds to create the smart coin called bitUSD without a MCFR
+         ///////
+         const optional<uint16_t> mcfr_null_opt = {};
+         {
+            const asset_create_operation create_op = make_bitasset("USDBIT", assetowner.id, market_fee_percent,
+                                                                   charge_market_fee, 4, core_id,
+                                                                   GRAPHENE_MAX_SHARE_SUPPLY, icr_opt, mcfr_null_opt);
+
+            trx.clear();
+            trx.operations.push_back(create_op);
+            sign(trx, assetowner_private_key);
+            PUSH_TX(db, trx); // No exception should be thrown
+         }
+
+         generate_block();
+         set_expiration(db, trx);
+         trx.clear();
+
+         const asset_object &bitusd = get_asset("USDBIT");
+         core = core_id(db);
+
+         // The force MCFR should not be set
+         BOOST_CHECK(!bitusd.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+
+
+         ///////
+         // 4. Asset owner fails to update the smart coin with a MCFR
+         ///////
+         const uint16_t mcfr_3 = 30; // 3% MCFR (BSIP74)
+         asset_update_bitasset_operation uop;
+         uop.issuer = assetowner.id;
+         uop.asset_to_update = bitusd.get_id();
+         uop.new_options = bitusd.bitasset_data(db).options;
+         uop.new_options.extensions.value.margin_call_fee_ratio = mcfr_3;
+
+         trx.clear();
+         trx.operations.push_back(uop);
+         db.current_fee_schedule().set_fee(trx.operations.back());
+         sign(trx, assetowner_private_key);
+         REQUIRE_EXCEPTION_WITH_TEXT(PUSH_TX(db, trx), "cannot be set before Hardfork BSIP74");
+
+         // The MCFR should not be set
+         BOOST_CHECK(!bitusd.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+
+
+         ///////
+         // 5. Asset owner fails to update the smart coin with a MCFR in a proposal
+         ///////
+         {
+            proposal_create_operation cop;
+            cop.review_period_seconds = 86400;
+            uint32_t buffer_seconds = 60 * 60;
+            cop.expiration_time = db.head_block_time() + *cop.review_period_seconds + buffer_seconds;
+            cop.fee_paying_account = GRAPHENE_TEMP_ACCOUNT;
+            cop.proposed_ops.emplace_back(uop);
+
+            trx.clear();
+            trx.operations.push_back(cop);
+            // sign(trx, assetowner_private_key);
+            REQUIRE_EXCEPTION_WITH_TEXT(PUSH_TX(db, trx), "cannot be set before Hardfork BSIP74");
+
+            // The MCFR should not be set
+            BOOST_CHECK(!bitusd.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+         }
+
+
+         ///////
+         // 6. Activate HARDFORK_CORE_BSIP74_TIME
+         ///////
+         BOOST_CHECK(db.head_block_time() < HARDFORK_CORE_BSIP74_TIME); // Confirm still before hardfork activation
+         BOOST_TEST_MESSAGE("Advancing past Hardfork BSIP74");
+         generate_blocks(HARDFORK_CORE_BSIP74_TIME);
+         generate_block();
+         set_expiration(db, trx);
+         trx.clear();
+
+
+         ///////
+         // 7. Asset owner succeeds to create the smart coin called CNYBIT with a MCFR
+         ///////
+         {
+            mcfr_opt = mcfr_3;
+            const asset_create_operation create_op = make_bitasset("CNYBIT", assetowner.id, market_fee_percent,
+                                                                   charge_market_fee, 4, core_id,
+                                                                   GRAPHENE_MAX_SHARE_SUPPLY, icr_opt, mcfr_opt);
+
+            trx.clear();
+            trx.operations.push_back(create_op);
+            sign(trx, assetowner_private_key);
+            PUSH_TX(db, trx); // No exception should be thrown
+         }
+
+         generate_block();
+         set_expiration(db, trx);
+         trx.clear();
+
+         const auto &bitcny = get_asset("CNYBIT");
+
+         // The MCFR should be set
+         BOOST_CHECK(bitcny.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+         BOOST_CHECK_EQUAL(*bitcny.bitasset_data(db).options.extensions.value.margin_call_fee_ratio, mcfr_3);
+
+
+         ///////
+         // 8. Asset owner succeeds to create the smart coin called RUBBIT with a MCFR in a proposal
+         ///////
+         const uint16_t mcfr_1 = 10; // 1% expressed in terms of GRAPHENE_COLLATERAL_RATIO_DENOM
+         {
+            // Create the proposal
+            mcfr_opt = mcfr_1;
+            const asset_create_operation create_op = make_bitasset("RUBBIT", assetowner.id, market_fee_percent,
+                                                                   charge_market_fee, 4, core_id,
+                                                                   GRAPHENE_MAX_SHARE_SUPPLY, icr_opt, mcfr_opt);
+
+            proposal_create_operation cop;
+            cop.review_period_seconds = 86400;
+            uint32_t buffer_seconds = 60 * 60;
+            cop.expiration_time = db.head_block_time() + *cop.review_period_seconds + buffer_seconds;
+            cop.fee_paying_account = GRAPHENE_TEMP_ACCOUNT;
+            cop.proposed_ops.emplace_back(create_op);
+
+            trx.clear();
+            trx.operations.push_back(cop);
+            // sign(trx, assetowner_private_key);
+            processed_transaction processed = PUSH_TX(db, trx);
+
+
+            // Approve the proposal
+            proposal_id_type pid = processed.operation_results[0].get<object_id_type>();
+
+            proposal_update_operation pup;
+            pup.fee_paying_account = assetowner_id;
+            pup.proposal = pid;
+            pup.active_approvals_to_add.insert(assetowner_id);
+            trx.clear();
+            trx.operations.push_back(pup);
+            set_expiration(db, trx);
+            sign(trx, assetowner_private_key);
+
+            PUSH_TX(db, trx); // No exception should be thrown
+
+
+            // Advance to the activation of the proposal
+            generate_blocks(cop.expiration_time);
+            set_expiration(db, trx);
+         }
+         const auto &bitrub = get_asset("RUBBIT");
+
+         // The MCFR should be set
+         BOOST_CHECK(bitrub.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+         BOOST_CHECK_EQUAL(*bitrub.bitasset_data(db).options.extensions.value.margin_call_fee_ratio, mcfr_1);
+
+
+         ///////
+         // 9. Asset owner succeeds to update the smart coin called CNYBIT with a MCFR
+         ///////
+         uop = asset_update_bitasset_operation();
+         uop.issuer = assetowner.id;
+         uop.asset_to_update = bitcny.get_id();
+         uop.new_options = bitcny.bitasset_data(db).options;
+         uop.new_options.extensions.value.margin_call_fee_ratio = mcfr_5;
+
+         trx.clear();
+         trx.operations.push_back(uop);
+         db.current_fee_schedule().set_fee(trx.operations.back());
+         sign(trx, assetowner_private_key);
+         PUSH_TX(db, trx);
+
+         // The MCFR should be set
+         BOOST_CHECK(bitcny.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+         BOOST_CHECK_EQUAL(*bitcny.bitasset_data(db).options.extensions.value.margin_call_fee_ratio, mcfr_5);
+
+
+         ///////
+         // 10. Asset owner succeeds to update the smart coin called RUBBIT with a MCFR in a proposal
+         ///////
+         {
+            // Create the proposal
+            uop = asset_update_bitasset_operation();
+            uop.issuer = assetowner.id;
+            uop.asset_to_update = bitrub.get_id();
+            uop.new_options = bitrub.bitasset_data(db).options;
+            uop.new_options.extensions.value.margin_call_fee_ratio = mcfr_5;
+
+            proposal_create_operation cop;
+            cop.review_period_seconds = 86400;
+            uint32_t buffer_seconds = 60 * 60;
+            cop.expiration_time = db.head_block_time() + *cop.review_period_seconds + buffer_seconds;
+            cop.fee_paying_account = GRAPHENE_TEMP_ACCOUNT;
+            cop.proposed_ops.emplace_back(uop);
+
+            trx.clear();
+            trx.operations.push_back(cop);
+            // sign(trx, assetowner_private_key);
+            processed_transaction processed = PUSH_TX(db, trx);
+
+
+            // Approve the proposal
+            proposal_id_type pid = processed.operation_results[0].get<object_id_type>();
+
+            proposal_update_operation pup;
+            pup.fee_paying_account = assetowner_id;
+            pup.proposal = pid;
+            pup.active_approvals_to_add.insert(assetowner_id);
+            trx.clear();
+            trx.operations.push_back(pup);
+            set_expiration(db, trx);
+            sign(trx, assetowner_private_key);
+
+            PUSH_TX(db, trx); // No exception should be thrown
+
+            // Advance to the activation of the proposal
+            generate_blocks(cop.expiration_time);
+            set_expiration(db, trx);
+         }
+
+         // The MCFR should be set
+         BOOST_CHECK(bitrub.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+         BOOST_CHECK_EQUAL(*bitrub.bitasset_data(db).options.extensions.value.margin_call_fee_ratio, mcfr_5);
+
+
+         ///////
+         // 11. Asset owner succeeds to create the smart coin called YENBIT without a MCFR
+         ///////
+         {
+            const asset_create_operation create_op = make_bitasset("YENBIT", assetowner.id, market_fee_percent,
+                                                                   charge_market_fee, 4, core_id,
+                                                                   GRAPHENE_MAX_SHARE_SUPPLY, icr_opt, mcfr_null_opt);
+
+            trx.clear();
+            trx.operations.push_back(create_op);
+            sign(trx, assetowner_private_key);
+            PUSH_TX(db, trx); // No exception should be thrown
+         }
+
+         generate_block();
+         set_expiration(db, trx);
+         trx.clear();
+
+         const auto &bityen = get_asset("YENBIT");
+
+         // The MCFR should be set
+         BOOST_CHECK(!bityen.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+
+
+         ///////
+         // 12. Asset owner succeeds to update the smart coin called RUBBIT without a MCFR in a proposal
+         ///////
+         {
+            // Create the proposal
+            uop = asset_update_bitasset_operation();
+            uop.issuer = assetowner.id;
+            uop.asset_to_update = bitrub.get_id();
+            uop.new_options = bitrub.bitasset_data(db).options;
+            uop.new_options.extensions.value.margin_call_fee_ratio = mcfr_null_opt;
+
+            proposal_create_operation cop;
+            cop.review_period_seconds = 86400;
+            uint32_t buffer_seconds = 60 * 60;
+            cop.expiration_time = db.head_block_time() + *cop.review_period_seconds + buffer_seconds;
+            cop.fee_paying_account = GRAPHENE_TEMP_ACCOUNT;
+            cop.proposed_ops.emplace_back(uop);
+
+            trx.clear();
+            trx.operations.push_back(cop);
+            // sign(trx, assetowner_private_key);
+            processed_transaction processed = PUSH_TX(db, trx);
+
+
+            // Approve the proposal
+            proposal_id_type pid = processed.operation_results[0].get<object_id_type>();
+
+            proposal_update_operation pup;
+            pup.fee_paying_account = assetowner_id;
+            pup.proposal = pid;
+            pup.active_approvals_to_add.insert(assetowner_id);
+            trx.clear();
+            trx.operations.push_back(pup);
+            set_expiration(db, trx);
+            sign(trx, assetowner_private_key);
+
+            PUSH_TX(db, trx); // No exception should be thrown
+
+            // Advance to the activation of the proposal
+            generate_blocks(cop.expiration_time);
+            set_expiration(db, trx);
+         }
+
+         // The MCFR should NOT be set
+         BOOST_CHECK(!bitrub.bitasset_data(db).options.extensions.value.margin_call_fee_ratio.valid());
+
+
+      } FC_LOG_AND_RETHROW()
+   }
+
+
 BOOST_AUTO_TEST_SUITE_END()
